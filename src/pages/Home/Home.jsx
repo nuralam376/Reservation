e70bbab4179b25fbfa8b8a85cf8ffe1719ddeeae
Nur A Alam <nuraalam376@gmail.com>
2022-11-23 13:01:24 +0600

    Property list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../components/Featured/Featured";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import PropertyList from "../../components/PropertyList/PropertyList";
 import "./Home.css";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
         <div className="homeContainer">
           <Featured />
           <h1 className="homeTitle">Browse by property type</h1>
+          <PropertyList />
         </div>
     </>
   )
